c7b19c5359953ffe9674c2008283a7b4f9b2aefa
matheusvirissimo <matheus.virissimo@unesp.br>
2025-05-02 17:13:44 -0300

    feat: criação e visualização de cookie DTO

--- cookie.py
+++ cookie.py
@@ -7,6 +7,16 @@ def homepage():
     return "teste"
 
 
+@app.route("/criar_cookie")
+def criar_cookie():
+    resposta = make_response("Cookie foi criado") # pode retornar qualquer coisa dentro desse parâmetro
+    resposta.set_cookie("username", "mavinca") # é um dicionário do Python
+    return resposta
+
+@app.route("/ver_cookie")
+def ver_cookie(): 
+    cookies = request.cookies # vem em formato de dicionário    
+    return cookies
 
 if __name__ == "__main__":
     app.run(debug=True)
